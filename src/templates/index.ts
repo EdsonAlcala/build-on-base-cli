40d20ad69c7d8354bc6202b2f9450d6bcac80a5a
edsonalcala <edson_alcala@msn.com>
2023-12-15 02:38:23 +0100

    format

--- src/templates/index.ts
+++ src/templates/index.ts
@@ -2,11 +2,12 @@ export const TEMPLATES = {
   nextjs:
     'https://github.com/EdsonAlcala/nextjs-foundry/archive/refs/heads/main.zip',
   vite: 'https://github.com/EdsonAlcala/vite-foundry/archive/refs/heads/main.zip',
-  viteOpWagmi: 'https://github.com/EdsonAlcala/vite-foundry/archive/refs/heads/op-wagmi-beta-integration.zip'
+  viteOpWagmi:
+    'https://github.com/EdsonAlcala/vite-foundry/archive/refs/heads/op-wagmi-beta-integration.zip',
 }
 
 export const TEMPLATES_NAMES = {
   nextjs: 'NextJS + Foundry',
   vite: 'Vite + Foundry',
-  viteOpWagmi: 'Vite + Foundry + OpWagmi'
+  viteOpWagmi: 'Vite + Foundry + OpWagmi',
 }
